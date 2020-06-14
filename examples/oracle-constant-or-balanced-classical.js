fd3db54b340a1396344c97bf3e564ea0f17eedb8
Philip Weaver <simplygreatwork@gmail.com>
2020-06-14 11:39:21 -0500

    shove

--- examples/oracle-constant-or-balanced-classical.js
+++ examples/oracle-constant-or-balanced-classical.js
@@ -1,14 +1,22 @@
 
+
 const logger = require('../src/logger')()
 
-// a classical version of the basis for the Deutsch-Jozsa algorithm
-// a randomly chosen single bit classical oracle which returns either a constant result or balanced result for each input 0 or 1
-// the oracle must be tested at least once per valid input (0 or 1) to determine whether the function is constant or balanced
-// a 10-bit version of this classical oracle would need to test each value from 0 1023 (2^10) 
-// the quantum version can perform this task with a single run using superposition for any number of bits
+// a classical background for the Deutsch-Jozsa algorithm
+// suppose that an oracle contains a list of values either 0 or 1
+// if every element of the oracle returns 0, the oracle is constant
+// if every element of the oracle returns 1, the oracle is constant
+// if half of the elements return 0 and half return 1, the oracle is balanced
+// otherwise, the oracle is unbalanced
+// to detect whether constant or balanced, a 10-bit oracle would need to be queried a total of 1024 times (2^10)
+// but the quantum version can perform this task with a single run using superposition for any number of bits
 // todo: the classical and quantum versions of this algorithm need to be runnable as 2^n
-// todo: so convert this example to demo multiple bits
 
+run()
+run()
+run()
+run()
+run()
 run()
 run()
 run()
@@ -17,37 +25,45 @@ run()
 
 function run() {
 	
-	let oracle = Oracle().random()
-	let box = new Box(oracle)
-	box.test(0)
-	box.test(1)
+	let box = new Box()
+	let oracle = Oracle().random({ bits : 3})
+	box.evaluate(oracle)
 	let kind = box.kind()
-	console.log(`The kind of oracle detected was "${kind}".`)
+	console.log(`The kind of oracle detected was "${kind}". [${box.tally}]`)
 	console.log(`Was the kind of oracle detected correctly? : ${oracle.confirm(kind)}`)
 	console.log('')
 }
 
-function Box(oracle) {
-	
-	this.oracle = oracle
+function Box() {
 	
 	Object.assign(this, {
 		
-		test: function(input) {
+		evaluate: function(oracle) {
 			
-			this.results = this.results || []
-			this.results[input] = this.oracle.test(input)
+			this.tally = [0, 0]
+			repeat(oracle.size, function(index) {
+				let result = oracle.test(index)
+				this.tally[result] = this.tally[result] + 1
+			}.bind(this))
 		},
 		
 		is_constant: function() {
-			return this.results[0] === this.results[1]
+			return this.tally[0] === 0 || this.tally[1] === 0
 		},
 		
 		is_balanced: function() {
-			return (this.results[0] === 0 && this.results[1] === 1) || (this.results[0] === 1 && this.results[1] === 0)
+			return this.tally[0] === this.tally[1]
 		},
 		
 		kind: function() {
+			
+			if (this.is_constant()) {
+				return 'constant'
+			} else if (this.is_balanced()) {
+				return 'balanced'
+			} else {
+				return 'unbalanced'
+			}
 			
 			if (false) {
 				return this.is_constant() ? 'constant' : 'balanced'
@@ -62,23 +78,35 @@ function Oracle() {
 	
 	return {
 		
-		random: function() {
+		random: function(options) {
 			
+			let size = Math.pow(2, options.bits)
 			let oracles = [{
+				size: size,
 				test: function(value) { return 0 },
 				confirm: function(kind) { return kind == 'constant' }
 			}, {
+				size: size,
 				test: function(value) {	return 1 },
 				confirm: function(kind) { return kind == 'constant' }
 			}, {
-				test: function(value) { return value },
+				size: size,
+				test: function(value) { return value % 2 },
 				confirm: function(kind) { return kind == 'balanced' }
 			}, {
-				test: function(value) { return 1 - value },
-				confirm: function(kind) { return kind == 'balanced' }
+				size: size,
+				test: function(value) { return [0, 0, 1][value % 3]},		// issue: single bit will always be constant or balanced
+				confirm: function(kind) { return kind == 'unbalanced' }
 			}]
 			
 			return oracles[Math.floor(Math.random() * 4)]
 		}
+	}
+}
+
+function repeat(number, fn) {
+	
+	for (let i = 0; i < number; i++) {
+		fn.apply(this, [i])
 	}
 }
