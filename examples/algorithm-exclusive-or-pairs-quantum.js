41b0ef847dff1a72da6fd89535325e3679dabb11
Philip Weaver <simplygreatwork@gmail.com>
2020-06-18 23:52:16 -0500

    shove

--- examples/algorithm-exclusive-or-pairs-quantum.js
+++ examples/algorithm-exclusive-or-pairs-quantum.js
@@ -6,14 +6,14 @@ const Bits = require('../src/bits')
 // a work in progress - the current implementation uses 2 qubits
 // todo: render the oracle gates dynamically for a larger number of qubits
 
-repeat(1, function() {
+repeat(10, function() {
 	run()
 })
 
 function run() {
 	
 	let host = new Host()
-	let oracle = new Oracle({ length: 2 })
+	let oracle = new Oracle({ length: 3 })
 	let result = host.test(oracle)
 	logger.log('')
 	logger.log(`The host has detected a result of ${result}`)
@@ -72,19 +72,118 @@ function Oracle(options) {
 		},
 		
 		apply: function(circuit) {
-			return this['apply_' + this.secret](circuit)
+			
+			if (true) {
+				this.apply_dynamically(circuit)
+			} else {
+				this['apply_' + this.secret](circuit)
+			}
 		},
 		
-		apply_11: function(circuit) {
-			return circuit.cx(2, 0).cx(3, 0).cx(2, 1).cx(3, 1)
+		apply_dynamically: function(circuit) {
+			
+			Bits.fromString(this.secret).iterate(function(bit, index) {
+				if (bit) {
+					repeat(this.length, function(index_) {
+						let target = this.length + index_
+						circuit.cx(target, index)
+					}.bind(this))
+				}
+			}.bind(this))
 		},
 		
 		apply_01: function(circuit) {
-			return circuit.cx(2, 0).cx(3, 0)
+			
+			return circuit
+			.cx(2, 0)
+			.cx(3, 0)
 		},
 		
 		apply_10: function(circuit) {
-			return circuit.cx(2, 1).cx(3, 1)
+			
+			return circuit
+			.cx(2, 1)
+			.cx(3, 1)
+		},
+		
+		apply_11: function(circuit) {
+			
+			return circuit
+			.cx(2, 0)
+			.cx(3, 0)
+			.cx(2, 1)
+			.cx(3, 1)
+		},
+		
+		apply_001: function(circuit) {
+			
+			return circuit
+			.cx(3, 0)
+			.cx(4, 0)
+			.cx(5, 0)
+		},
+		
+		apply_010: function(circuit) {
+			
+			return circuit
+			.cx(3, 1)
+			.cx(4, 1)
+			.cx(5, 1)
+		},
+		
+		apply_011: function(circuit) {
+			
+			return circuit
+			.cx(3, 0)
+			.cx(4, 0)
+			.cx(5, 0)
+			.cx(3, 1)
+			.cx(4, 1)
+			.cx(5, 1)
+		},
+		
+		apply_100: function(circuit) {
+			
+			return circuit
+			.cx(3, 2)
+			.cx(4, 2)
+			.cx(5, 2)
+		},
+		
+		apply_101: function(circuit) {
+			
+			return circuit
+			.cx(3, 0)
+			.cx(4, 0)
+			.cx(5, 0)
+			.cx(3, 2)
+			.cx(4, 2)
+			.cx(5, 2)
+		},
+		
+		apply_110: function(circuit) {
+			
+			return circuit
+			.cx(3, 1)
+			.cx(4, 1)
+			.cx(5, 1)
+			.cx(3, 2)
+			.cx(4, 2)
+			.cx(5, 2)
+		},
+		
+		apply_111: function(circuit) {
+			
+			return circuit
+			.cx(3, 0)
+			.cx(4, 0)
+			.cx(5, 0)
+			.cx(3, 1)
+			.cx(4, 1)
+			.cx(5, 1)
+			.cx(3, 2)
+			.cx(4, 2)
+			.cx(5, 2)
 		},
 		
 		confirm: function(value) {
