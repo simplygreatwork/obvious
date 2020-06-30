2e35346fb8bb64b34e0fd85e82eefb547cb8778d
Philip Weaver <simplygreatwork@gmail.com>
2020-06-29 22:11:42 -0500

    shove

--- examples/phase-estimation.js
+++ examples/phase-estimation.js
@@ -2,31 +2,35 @@
 const logger = require('../src/logger')()
 const Bits = require('../src/bits')
 
-estimate(1)
+estimate({ degree: 45, size: 3 })
+estimate({ degree: 22.5, size: 4 })
+estimate({ degree: 11.25, size: 5 })
 
-function estimate(value) {
+function estimate(options) {
 	
-	input(value)
-	output(value)
+	input(options.degree, options.size)
+	output(options.degree, options.size)
 }
 
-function input(value) {
+function input(degree, size) {
 	
-	let size = 3
-	Circuit(`input for phase estimation of ${value} / ${Math.pow(2, size)}`, size + 1)
-	.unit(0, 3).h().circuit()
-	.encode(value)
+	Circuit(`input for phase estimation of ${degree} degrees with a register unit precision of ${Math.pow(2, size)}`, size + 1)
+	.unit(0, size).h().circuit()
+	.encode(degree)
 	.run()
 }
 
-function output(value) {
+function output(degree, size) {
 	
-	let size = 3
-	Circuit(`input for phase estimation of ${value} / ${Math.pow(2, size)}`, size + 1)
-	.unit(0, 3).h().circuit()
-	.encode(value)
-	.qft_inverse()
+	let circuit = Circuit(`output for phase estimation of ${degree} degrees with a register unit precision of ${Math.pow(2, size)}`, size + 1)
+	let unit = circuit.unit(0, size)
+	unit.h().circuit()
+	.encode(degree)
+	.qft_inverse(0, size)
 	.run()
+	let result = unit.measure().toNumber()
+	degree = 360 * (result / Math.pow(2, size))
+	logger.log(`The estimated phase is ${result} / ${Math.pow(2, size)} of 360 degrees which is ${degree} degrees.`)
 }
 
 function Circuit(name, size) {
@@ -41,18 +45,20 @@ function Circuit(name, size) {
 	
 	return Object.assign(circuit, {
 		
-		encode: function(value) {
+		encode: function(degree) {
 			
 			let reps = 1
 			let unit = this.unit(0, this.size - 1)
+			let value = (Math.pow(2, unit.length) * degree) / 360
 			repeat(unit.length, function(index) {
 				let target = unit.length
 				let control = index
-				this.crz(target, control, { phi : `${value} * -pi / 8 * ${reps}`})
+				let increment = Math.pow(2, unit.length)
+				this.crz(target, control, { phi : `${value} * -pi / ${increment} * ${reps}`})
 				this.cx(target, control)
-				this.crz(target, control, { phi : `${value} * -pi / 4 * ${reps}`})
+				this.crz(target, control, { phi : `${value} * -pi / ${increment / 2} * ${reps}`})
 				this.cx(target, control)
-				this.crz(target, control, { phi : `${value} * -pi / 8 * ${reps}`})
+				this.crz(target, control, { phi : `${value} * -pi / ${increment} * ${reps}`})
 				reps = reps * 2
 			}.bind(this))
 			return this
@@ -60,14 +66,18 @@ function Circuit(name, size) {
 		
 		qft_inverse: function(begin, length) {
 			
+			begin = begin || 0
+			length = length || this.size
+			for (let i = begin, length_ = Math.floor((begin + length) / 2); i < length_; i++) {
+				this.swap(i, length - (i + 1))
+			}
+			repeat(length, function(index) {
+				this.h(begin + index)
+				for (let j = index + 1; j < length; j++) {
+					this.cu1(j, index, { lambda: 'pi / ' + Math.pow(2, j - index) })
+				}
+			}.bind(this))
 			return this
-			.swap(0, 2)
-			.h(0)
-			.crz(0, 1, { phi: 'pi / 2' })
-			.crz(0, 2, { phi: 'pi / 4' })
-			.h(1)
-			.crz(1, 2, { phi: 'pi / 2' })
-			.h(2)
 		}
 	})
 }
