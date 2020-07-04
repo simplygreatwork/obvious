6aaa55b7ee74717348b7434bc3154dccfe8a3e3e
Philip Weaver <simplygreatwork@gmail.com>
2020-07-04 17:40:51 -0500

    shove

--- examples/reversibility.js
+++ examples/reversibility.js
@@ -1,2 +1,30 @@
 
 const logger = require('../src/logger')()
+
+circuit('applying and reversing a haddamard gate', 1)
+.h(0)
+.h(0)
+.run('trace')
+
+circuit('applying and reversing a pauli-x (not) gate', 1)
+.x(0)
+.x(0)
+.run('trace')
+
+circuit('applying and reversing a phase change', 1)
+.x(0)
+.u1(0, [], { lambda: 'pi / 4' })
+.u1(0, [], { lambda: '-pi / 4' })
+.x(0)
+.run('trace')
+
+function circuit(name, size) {
+	
+	return require('../src/circuit.js')({
+		name: name,
+		size: size,
+		logger: logger,
+		engine: 'optimized',
+		order: ['targets', 'controls']
+	})
+}
