ffb23e77e98d80c703345cffced7b413560bafa9
Philip Weaver <simplygreatwork@gmail.com>
2020-07-04 17:04:11 -0500

    shove

--- examples/phase-relative.js
+++ examples/phase-relative.js
@@ -1,6 +1,11 @@
 
 const logger = require('../src/logger')()
 
+circuit('a quarter turn (90 degree) relative phase change in super position with an s gate', 1)
+.h(0)
+.s(0)
+.run()
+
 circuit('a quarter turn (90 degree) relative phase change in super position with ui gate', 1)
 .h(0)
 .u1(0, [], { lambda: 'pi / 2' })
@@ -9,11 +14,6 @@ circuit('a quarter turn (90 degree) relative phase change in super position with
 circuit('a quarter turn (90 degree) relative phase change in super position with an rz gate', 1)
 .h(0)
 .rz(0, [], { phi: 'pi / 2' })
-.run()
-
-circuit('a quarter turn (90 degree) relative phase change in super position with an s gate', 1)
-.h(0)
-.s(0)
 .run()
 
 function circuit(name, size) {
